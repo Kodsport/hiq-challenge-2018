b673be31340519db544e636fede23de89b41a3cc
Simon Lindholm <simon.lindholm10@gmail.com>
2018-10-22 01:04:53 +0200

    [isithalloween] solution, statement fix

---
+++ isithalloween/submissions/accepted/sl.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.sync_with_stdio(0); cin.tie(0);
+	cin.exceptions(cin.failbit);
+	string line;
+	getline(cin, line);
+	if (line == "OCT 31" || line == "DEC 25") {
+		cout << "yup\n";
+	} else {
+		cout << "nope\n";
+	}
+}
